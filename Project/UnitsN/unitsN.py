b3f9990553e0961777f97af881603c94a4bdecc6
0x07CB <83157348+0x07CB@users.noreply.github.com>
2021-10-25 16:11:06 +0000

    update again to construct this piece of the machine...

--- Project/UnitsN/unitsN.py
+++ Project/UnitsN/unitsN.py
@@ -61,7 +61,26 @@ else:
 #
 class unitsN(object):
     def __init__(self):
+        self.powerOf = 10
         self.strUnitNum = "0"
+        # Units data prepared for the computes
+        self.unitsLetters = ["","K","M","G","T","P"]
+        self.unitsValuePowerOf = [0,3,6,9,12,15]
+        # Units data for conversion and compute 
+
+
+
+
+
+    
+    def calcPowerOf(self, x, uPow):
+        try:
+            return x * ( self.powerOf ** uPow ) 
+        except Exception as e:
+            print("Exception is : {e}".format(
+                e = e
+                ))
+            exit(-1)
 
 
     def callErrorShowFunction(self, TYPE_ERR_DESC, ARG_ERR_NAME=""):
@@ -78,8 +97,18 @@ class unitsN(object):
 
 uNum = unitsN()
 
-# ERROR ON ARG, MANAGEMENT
 if args.units:
+    # just N2U/U2N
+    if (len(units) == 1):
+        uNum.numToUnits(num,units)
+
+        uNum.unitsToNum(units,num)
+
+    # maybe U2U
+    elif (len(units) == 2):
+        pass
+
+    # errors managed...
     elif (len(units) > 2):
         uNum.callErrorShowFunction("TOO MANY UNITS ON ARGS:", "'--units'")
     elif (len(units) < 1):
